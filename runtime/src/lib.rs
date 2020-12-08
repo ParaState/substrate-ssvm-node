3243c30163b6fa164fc923a63281924c8f774e8b
Yi Huang <yi@secondstate.io>
2020-12-08 07:18:29 +0000

    [Fix] substrate#5761 s/TransactionBaseFee//g

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -211,14 +211,12 @@ impl balances::Trait for Runtime {
 }
 
 parameter_types! {
-    pub const TransactionBaseFee: Balance = 0;
     pub const TransactionByteFee: Balance = 1;
 }
 
 impl transaction_payment::Trait for Runtime {
     type Currency = balances::Module<Runtime>;
     type OnTransactionPayment = ();
-    type TransactionBaseFee = TransactionBaseFee;
     type TransactionByteFee = TransactionByteFee;
     type WeightToFee = ConvertInto;
     type FeeMultiplierUpdate = ();
